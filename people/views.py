3ce2bc921095ef3130ed76fd8807999d213e8a60
Maksim Baturin <baturinmaksim188@gmail.com>
2023-04-11 13:56:14 +0600

    Creating a website with bulletin board functions and creating them not through the admin panel

---
+++ people/views.py
@@ -0,0 +1,81 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .forms import *
+from .models import *
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
+
+
+def index(request):
+    posts = People.objects.all()
+
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
+
+    return render(request, 'people/index.html', context=context)
+
+
+def about(request):
+    return render(request, 'people/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def addpage(request):
+    if request.method == 'POST':
+        form = AddPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            form.save()
+            return redirect('home')
+    else:
+        form = AddPostForm()
+    return render(request, 'people/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+def show_post(request, post_slug):
+    post = get_object_or_404(People, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'people/post.html', context=context)
+
+
+def show_category(request, cat_id):
+    posts = People.objects.filter(cat_id=cat_id)
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'people/index.html', context=context)
